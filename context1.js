0a0b24f736721544107926157cfff2cd529ad357
DarkSoul-dev <lovekush098@privatemail.com>
2020-07-25 19:49:11 +0530

    context p-2

---
+++ context1.js
@@ -0,0 +1,23 @@
+function tipper(a){
+    var bill = parseInt(a);
+    console.log(bill + 2);
+}
+
+tipper("6");
+
+function bigTipper(a){
+    var bill = parseInt(a);
+    console.log(bill + 15);
+}
+
+bigTipper("200");
+
+console.log(name);
+var name = "LoveKush";
+
+function sayName() {
+    var name = "DarkSoul";
+    console.log(name);
+}
+sayName();
+console.log(name);
