c5caf07e803594ef8300137b299fbf5b44d8772c
DarkSoul-dev <lovekush098@privatemail.com>
2020-07-31 21:03:54 +0530

    slice and splice

---
+++ filterfill.js
@@ -0,0 +1,9 @@
+var testArray = [2, 4, 6, 7, 8, 9, 0, 3, 1];
+
+console.log(testArray.fill("L",2,6));
+
+const myNumber = [23, 45, 78, 90, 65 ,43, 21];
+
+const result = myNumber.filter((num) => num!= 21 );
+
+console.log(result);
---
+++ slicesplice.js
@@ -0,0 +1,6 @@
+var user = ["Ted", "Tim", "God", "DarkSoul", "Devil", "godess"];
+
+console.log(user.slice(1));
+
+user.splice(1,4, "Hi", "BYE");
+console.log(user);
