bf6ff1fef3da11b5f025e963185b4f6be3de2a53
DarkSoul-dev <lovekush098@privatemail.com>
2020-08-02 19:50:20 +0530

    topice object and roles

---
+++ object.js
@@ -0,0 +1,12 @@
+var user = {
+    firstName : "Darksoul",
+    lastName : "angles",
+    role : "Admin",
+    loginCount : 34,
+    googleSingedIn : true
+};
+
+console.log(user.googleSingedIn);
+console.log(user["lastName"]);
+
+console.table(user);
---
+++ object1.js
@@ -0,0 +1,11 @@
+var iphone = {
+    relesedate : 08/09/2020,
+    os : "ios14",
+    model: "iphone12",
+    ram : "sixgb",
+    camera : "twelvemp",
+    display : "sixinch",
+    price : "SeventyFiveThousand"
+};
+
+console.table(iphone);
