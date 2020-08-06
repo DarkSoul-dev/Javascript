84f9fbb9913f01674398132eabd70351f8731076
DarkSoul-dev <lovekush098@privatemail.com>
2020-08-06 20:47:05 +0530

    loop1 and loop2

---
+++ loop1.js
@@ -0,0 +1,26 @@
+// for( let i = 0; i <= 10; i++){
+//     console.log(i);
+// }
+
+
+const myStates = ["Himachal",
+ "Punjab",
+  "Assam",
+  1947,
+   "Tamil NAdu",
+    "Maharashtra",
+];
+
+// for (let i = 0; i< myStates.length; i++){
+//     if(typeof myStates[i] !== "string") continue;
+//     console.log(myStates[i]);
+// }
+
+let i = 0;
+
+do {
+    console.log(myStates[i]);
+    i++;
+}
+
+    while(i < 10);
---
+++ loop2.js
@@ -0,0 +1,18 @@
+const myStates = [
+    "Punjab",
+    "Assam",
+    "Tamil nAdu",
+    1987,
+    "Maharashtra",
+    "Delhi",
+    "Mumbai",
+];
+
+// var i = 0
+// for (;;){
+//     if(i > 3) break;
+//     console.log(i);
+//     i++;
+// }
+
+myStates.forEach((s) => (console.log(s)));
