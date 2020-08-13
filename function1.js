5921e39bef9777b0564b75ec416625d26c88a808
DarkSoul-dev <lovekush098@privatemail.com>
2020-08-13 19:50:07 +0530

    change cote into backtiks

--- function1.js
+++ function1.js
@@ -2,22 +2,28 @@ var getUserRole = function getUserRole(name, role) {
 switch (role)
 {
 case "admin":
-return '${name} is admin with all access';
+return `${name} is admin with all access`;
 break;
+
 case "subadmin":
-return '${name} is sub-admin with access to create and delete courses';
+return `${name} is sub-admin with access to create and delete courses`;
 break;
+
 case"testprep":
-return '${name}is atest prep with access to create and delete courses';
+return `${name} is a test prep with access to create and delete courses`;
 break;
+
 case "user":
-return '${name} is a user to consume content';
+return `${name} is a user to consume content`;
 break;
+
 default:
-return '${name} is trial user';
+return `${name} is trial user`;
 break;
+
+  }
 }
-}
-console.log(getUserRole("lovekush","testprep"));
-var getRole = getUserRole("love","user");
+
+console.log(getUserRole("lovekush","admin"));
+var getRole = getUserRole("sushil","subadmin");
 console.log(getRole); 
